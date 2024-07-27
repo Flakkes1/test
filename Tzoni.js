0828eeacfb79f40da7d9102e60f9161edadf0f1a
Flakkes1 <166268285+Flakkes1@users.noreply.github.com>
2024-07-27 22:33:10 +0100

    Update Tzoni.js

--- Tzoni.js
+++ Tzoni.js
@@ -13,29 +13,40 @@
 
 FUCK YOU
 
-    // Current script version
+(function() {
     var currentVersion = '0.3';
 
-    // Function to check for updates
     function checkForUpdate() {
         GM_xmlhttpRequest({
             method: 'GET',
             url: 'https://raw.githubusercontent.com/Flakkes1/test/main/Tzoni.js',
             onload: function(response) {
                 var remoteVersionMatch = /@version\s+([\d.]+)/.exec(response.responseText);
-                if (remoteVersionMatch && remoteVersionMatch[1] && remoteVersionMatch[1] !== currentVersion) {
-                    alert('A new version of the script is available. The page will now reload to update.');
-                    setTimeout(function() {
-                        window.location.reload();
-                    }, 3000); // Wait 3 seconds before reloading to allow alert to be seen
+                if (remoteVersionMatch) {
+                    var remoteVersion = remoteVersionMatch[1];
+                    if (remoteVersion && remoteVersion !== currentVersion) {
+                        console.log('A new version (' + remoteVersion + ') of the script is available. Current version is ' + currentVersion + '.');
+                        alert('A new version of the script is available. The page will now reload to update.');
+                        setTimeout(function() {
+                            GM_setValue('isUpdated', true);  // Set a flag indicating that an update is happening
+                            window.location.reload();
+                        }, 3000);  // Wait 3 seconds before reloading to allow alert to be seen
+                    }
                 }
+            },
+            onerror: function(error) {
+                console.log('Error checking for update:', error);
             }
         });
     }
 
-    // Check for updates every 30 minutes
-    setInterval(checkForUpdate, 5 * 1000);
-
-    // Initial check on script load
-    checkForUpdate();
+    // Only check for updates if not in the middle of an update
+    if (!GM_getValue('isUpdated', false)) {
+        setInterval(checkForUpdate, 5 * 1000);  // Check for updates every 5 seconds (for testing purposes)
+        checkForUpdate();  // Initial check on script load
+    } else {
+        GM_setValue('isUpdated', false);  // Reset the flag after reload
+        console.log('Script updated to new version');
+    }
 })();
+
