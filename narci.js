e1baf6fe6b98533ef30a762e1646d32d274d10fd
Flakkes1 <166268285+Flakkes1@users.noreply.github.com>
2024-08-27 02:35:05 +0100

    Update narci.js

--- narci.js
+++ narci.js
@@ -19,7 +19,7 @@ const cast_spell_city = (town_id, favors) => {
         "action_name": "cast",
         "arguments": {
             "power_id": "wedding",
-            "target_id": 4233
+            "target_id": 4235
         },
         "nl_init": true
     };
